1258922b58c980ceda5cf45a42502580df6f5d3c
Valentina Lugovaia <vaal.it@yandex.ru>
2023-06-01 12:43:01 +0300

    final edits

--- app/js/index.js
+++ app/js/index.js
@@ -1,12 +1,24 @@
 const openPopup = document.querySelector('.slider__button_right');
 const popup = document.querySelector('.popup');
 const closePopup = document.querySelector('.popup__close-button');
+const openNavbar = document.querySelector('.header__burger-button');
+const navbar = document.querySelector('.header__nav-bar');
+const closeNavbar = document.querySelector('.header__close-button');
 
 openPopup.addEventListener('click', () => {
     popup.classList.add('popup_opened');
 });
 
-openPopup.addEventListener('click', () => {
+closePopup.addEventListener('click', () => {
     popup.classList.remove('popup_opened');
 });
+
+openNavbar.addEventListener('click', () => {
+    navbar.classList.add('header__nav-bar_opened');
+});
+
+closeNavbar.addEventListener('click', () => {
+    navbar.classList.remove('header__nav-bar_opened');
+});
+
 
